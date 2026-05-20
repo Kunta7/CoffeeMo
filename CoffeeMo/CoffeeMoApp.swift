1f87679056e90f57ed28d8c73a9d8b7132bca963
Kunta7 <Patiencelion.ap@gmail.com>
2026-05-20 23:26:14 +0200

    Initial Commit

---
+++ CoffeeMo/CoffeeMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMoApp.swift
+//  CoffeeMo
+//
+//  Created by MAC on 20.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMoTests/CoffeeMoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMoTests.swift
+//  CoffeeMoTests
+//
+//  Created by MAC on 20.05.2026.
+//
+
+import Testing
+@testable import CoffeeMo
+
+struct CoffeeMoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
